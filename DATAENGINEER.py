53aab977037df174bdbe172b53c5d60f7fa59139
Leela <arikatla.l.k.sarama@gmail.com>
2024-11-26 13:56:08 -0600

    success

---
+++ DATAENGINEER.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## DATAENGINEER
+# 
+# New notebook
+
+# In[1]:
+
+
+import pandas as ks
+kavi = { "name":["A","L","K","S"],
+"AGE":[25,26,27,28],
+"COLOR":["B","W","R","O"]}
+leela = ks.DataFrame(kavi)
+print(leela)
+
